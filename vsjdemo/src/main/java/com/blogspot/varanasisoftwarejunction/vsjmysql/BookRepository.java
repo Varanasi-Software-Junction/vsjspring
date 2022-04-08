317cd43d39a612c043ab5f2c505f06d70830945d
Champak's World <champaksworld@gmail.com>
2022-04-08 17:54:32 +0530

    Find and Count

--- vsjdemo/src/main/java/com/blogspot/varanasisoftwarejunction/vsjmysql/BookRepository.java
+++ vsjdemo/src/main/java/com/blogspot/varanasisoftwarejunction/vsjmysql/BookRepository.java
@@ -2,5 +2,10 @@ package com.blogspot.varanasisoftwarejunction.vsjmysql;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    Iterable<Book> findAllById(Optional<Integer> a);
+
+    Iterable<Book> findByBookName( Optional<String> a);
 }
