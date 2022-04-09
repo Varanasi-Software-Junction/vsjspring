7d84a03791a9cad0692898cd5a24850aa147dd3d
Champak's World <champaksworld@gmail.com>
2022-04-09 22:38:12 +0530

    Enabled Thymeleaf

--- vsjdemo/src/main/java/com/blogspot/varanasisoftwarejunction/vsjmysql/restcontrollers/GreetingController.java
+++ vsjdemo/src/main/java/com/blogspot/varanasisoftwarejunction/vsjmysql/restcontrollers/GreetingController.java
@@ -5,8 +5,10 @@ package com.blogspot.varanasisoftwarejunction.vsjmysql.restcontrollers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GreetingController {
@@ -18,4 +20,20 @@ public class GreetingController {
         return "greeting";
     }
 
+    @RequestMapping("/view")
+    public ModelAndView welcome() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("hello.html");
+        return modelAndView;
+    }
+
+
+
+
+    @GetMapping("/sayhello")
+    public
+    String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", "Champak");
+        return "hello";
+    }
 }
