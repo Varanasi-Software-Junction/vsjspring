c97e84137348e9fcb5dfac3e32f9ac9448ee0e15
Champak's World <champaksworld@gmail.com>
2022-04-09 20:45:09 +0530

    Added error mapping

---
+++ vsjdemo/src/main/java/com/blogspot/varanasisoftwarejunction/vsjmysql/restcontrollers/GreetingController.java
@@ -0,0 +1,21 @@
+package com.blogspot.varanasisoftwarejunction.vsjmysql.restcontrollers;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public @ResponseBody
+    String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
---
+++ vsjdemo/src/main/java/com/blogspot/varanasisoftwarejunction/vsjmysql/restcontrollers/MyErrorController.java
@@ -0,0 +1,20 @@
+package com.blogspot.varanasisoftwarejunction.vsjmysql.restcontrollers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public @ResponseBody
+
+    String handleError() {
+        //do something like logging
+        return "<h1>My Error</h1>";
+
+
+    }
+}
